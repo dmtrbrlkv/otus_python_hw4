ba51640839edf63c0782edc45ce09b4ac4d11f6d
Dmitry Burlakov <dmtrbrlkv@gmail.com>
2019-05-21 21:56:08 +0300

    fix

--- app/httpd.py
+++ app/httpd.py
@@ -7,7 +7,7 @@ from socket import AF_INET, SOCK_STREAM, socket
 from const import *
 from response import Response, CacheContent
 from request import Request
-from multiprocessing.dummy import Pool as TreadPool
+from multiprocessing.dummy import Pool as ThreadPool
 
 
 class Worker:
@@ -18,22 +18,26 @@ class Worker:
             logging.exception("Processing error:")
             pass
 
-    @classmethod
-    def process_connection(cls, socket, dir, cache=None):
-        data = cls.read_http_data(socket)
+    @staticmethod
+    def process_connection(socket, dir, cache=None):
+        data = Worker.read_http_data(socket)
+        if not data:
+            socket.close()
+            return
         request = Request(data)
         response = Response.get_response(request, dir, cache)
         response_text = response.to_binary()
         socket.sendall(response_text)
         socket.close()
 
-    @classmethod
-    def read_http_data(cls, socket):
+    @staticmethod
+    def read_http_data(socket):
         data = ""
         while True:
             r = socket.recv(SOCKET_PART_SIZE)
             data += r.decode("utf8")
             if len(r) < 1:
+                data = None
                 break
             if HTTP_END in r:
                 break
@@ -42,27 +46,12 @@ class Worker:
         return data
 
 
-class ThreadPool2():
-    def __init__(self, n, dir, cache=None):
-        self.queue = Queue(n * 2)
-        for _ in range(n):
-            Worker(self.queue, dir, cache).start()
-
-    def add_task(self, *args, **kwargs):
-        self.queue.put((args, kwargs))
-
-    def stop(self):
-        for _ in range(self.queue.maxsize):
-            self.add_task(STOP_TASK)
-        self.queue.join()
-
-
 class Server:
     def __init__(self, workers_count, root_directory, port):
         self.workers_count = workers_count
         self.root_directory = root_directory
         self.port = port
-        self.tread_pool = None
+        self.thread_pool = None
         self.socket = None
         self.cache = CacheContent()
 
@@ -72,7 +61,7 @@ class Server:
         if not os.path.exists(dir):
             raise FileExistsError(f"Path {dir} not found")
 
-        self.tread_pool = TreadPool(self.workers_count)
+        self.thread_pool = ThreadPool(self.workers_count)
 
         s = socket(AF_INET, SOCK_STREAM)
 
@@ -82,11 +71,11 @@ class Server:
         logging.info("Server started")
         while True:
             c, a = s.accept()
-            self.tread_pool.starmap_async(Worker(), [(c, dir, self.cache)])
+            self.thread_pool.starmap_async(Worker(), [(c, dir, self.cache)])
 
     def stop(self):
-        if self.tread_pool:
-            self.tread_pool.terminate()
+        if self.thread_pool:
+            self.thread_pool.terminate()
         if self.socket:
             self.socket.close()
 
